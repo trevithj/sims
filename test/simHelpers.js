261de14dcf0f67d0a7cd3c216c2f62113e33e2a8
John Trevithick <trevithj@gmail.com>
2024-12-17 15:34:12 +1300

    Fix to Mocha watch-mode

--- test/simHelpers.js
+++ test/simHelpers.js
@@ -5,11 +5,11 @@ function sortByT(a,b) {
 export function simQueue() {
     const array = [];
     return {
-        add: (t, value) => {
+        add(t, value) {
             array.push({ t, value });
             array.sort(sortByT);
         },
-        pop: (t) => {
+        pop(t) {
             if (array.length === 0) return [];
             if (array[0].t > t) return [];
             const result = [];
@@ -19,6 +19,7 @@ export function simQueue() {
             }
             return result;
         },
+        size: () =>array.length,
         toString: () => JSON.stringify(array.map(obj => {
             const { t, value } = obj;
             return `${t}: ${value}`;
--- test/simHelpers.spec.js
+++ test/simHelpers.spec.js
@@ -12,9 +12,11 @@ describe("simQueue", function() {
         assert.ok(typeof queue === "object", "Expected an object");
         assert.ok(typeof queue.pop === "function", "Expected: 'pop' method");
         assert.ok(typeof queue.add === "function", "Expected: 'add' method");
+        assert.ok(typeof queue.size === "function", "Expected: 'size' method");
 
         const theString = queue.toString();
         assert.equal(theString, "[]", "Expected: is stringable");
+        assert.equal(queue.size(), 0, "Expected: zero size");
     });
 
     it("should pop the correct values", function() {
@@ -23,17 +25,17 @@ describe("simQueue", function() {
         queue.add(123, "anything");
         queue.add(124, "X");
         queue.add(125, "Y");
-        console.log(queue.toString());
+        assert.equal(queue.size(), 3, "Expect three items");
         
         let result = queue.pop(121);
         assert.equal(toStr(result), "[]", "Expect empty array");
         
         result = queue.pop(123);
         assert.equal(toStr(result), "[\"anything\"]", "Expect populated array");
-        console.log(queue.toString());
+        assert.equal(queue.size(), 2, "Expect two items");
         
         result = queue.pop(126);
         assert.equal(toStr(result), "[\"X\",\"Y\"]", "Expect populated array");
-        console.log(queue.toString());
+        assert.equal(queue.size(), 0, "Expect empty queue");
     });
 });
